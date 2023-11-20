26d34e60ed4e7f5e4d6c7cf072819025a4d2feae
Sajal <mahatosajal65@gmail.com>
2023-11-20 21:18:41 +0530

    array creation

---
+++ Arrays/creation.java
@@ -0,0 +1,10 @@
+public class creation {
+    public static void main(String args[]){
+        // creating an array
+        // dataType arrayName[] = new dataType[size];
+        int marks[] = new int[50];
+        int number[] = {1,2,3};
+        int moreNumber[] = {4,5,6};
+        String fruits[] = {"Apple","Mango", "Orange"}; 
+    }
+}

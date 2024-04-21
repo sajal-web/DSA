3801f073ae0774d989a2f2b75d497f4cfafcca1f
Sajal <mahatosajal65@gmail.com>
2024-04-21 12:50:23 +0530

    Java Basics - Output in Java

--- Variables and Data Type/JavaBasics.java
+++ Variables and Data Type/JavaBasics.java
@@ -1,5 +1,7 @@
-public class JavaBasics{
-    public static void main(String args[]){
-        System.out.print("Hello , world...");
+public class JavaBasics {
+    public static void main(String args[]) {
+        System.out.print("Hello World");
+        // To print any output we use System.out.print()
+        // To go to next line we use ( "\n", println())
     }
 }
